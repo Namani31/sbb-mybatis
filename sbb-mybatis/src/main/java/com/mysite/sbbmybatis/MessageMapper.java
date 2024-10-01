c58378618634bbe4ac6c5a7f8fed33acea43520c
Namani <kwg531@naver.com>
2024-10-01 16:38:20 +0900

    Feat: 리스트 반환하는 `getMessagesLimit10()`을 인터페이스에 추가

--- sbb-mybatis/src/main/java/com/mysite/sbbmybatis/MessageMapper.java
+++ sbb-mybatis/src/main/java/com/mysite/sbbmybatis/MessageMapper.java
@@ -1,5 +1,7 @@
 package com.mysite.sbbmybatis;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 // 매핑 xml에 기재 된 sql을 호출하기 위한 인터페이스
@@ -7,4 +9,5 @@ import org.apache.ibatis.annotations.Mapper;
 public interface MessageMapper {
 	Message getMessageById(Integer id);
 	void insertMessage(Message message);
+	List<Message> getMessagesLimit10();
 }
