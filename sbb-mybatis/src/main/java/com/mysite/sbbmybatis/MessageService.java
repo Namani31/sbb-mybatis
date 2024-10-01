fd66d93885e03acbbc6de477e0b9be46659a2d5d
Namani <kwg531@naver.com>
2024-10-01 16:29:40 +0900

    Refactor: 오탈자 수정 (MesageService.java -> MessageService.java)

--- sbb-mybatis/src/main/java/com/mysite/sbbmybatis/MessageService.java
+++ sbb-mybatis/src/main/java/com/mysite/sbbmybatis/MessageService.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MesageService {
+public class MessageService {
 	@Autowired
 	private MessageMapper messageMapper;
 	
