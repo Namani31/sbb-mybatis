a5ee411e47de6a65f60a30aa4ba180760770120b
Namani <kwg531@naver.com>
2024-10-01 15:11:12 +0900

    Feat: id와 message 값을 저장하고 읽는 `Message.java` 추가

---
+++ sbb-mybatis/src/main/java/com/mysite/sbbmybatis/Message.java
@@ -0,0 +1,18 @@
+package com.mysite.sbbmybatis;
+
+public class Message {
+	private Integer id; // 1
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	private String message; // Hello 
+}
