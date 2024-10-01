f7f738ec1c9c6e4e6e74918c45ce16c92c9617c7
Namani <kwg531@naver.com>
2024-10-01 15:26:25 +0900

    Feat: `@Mapper` 사용하기 위한 `MessageMapper.java` ㅊ]]\\]

---
+++ sbb-mybatis/src/main/java/com/mysite/sbbmybatis/MessageMapper.java
@@ -0,0 +1,9 @@
+package com.mysite.sbbmybatis;
+
+import org.apache.ibatis.annotations.Mapper;
+
+// 매핑 xml에 기재 된 sql을 호출하기 위한 인터페이스
+@Mapper
+public class MessageMapper {
+	Message getMessageById(Integer id);
+}
