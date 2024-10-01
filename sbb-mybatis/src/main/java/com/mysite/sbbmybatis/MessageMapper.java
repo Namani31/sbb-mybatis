fbed38960c3b54403268120dbe44cd2e9eb89c6c
Namani <kwg531@naver.com>
2024-10-01 16:31:45 +0900

    Modify: `MessageMapper.java`에서 class를 interface로 변경

--- sbb-mybatis/src/main/java/com/mysite/sbbmybatis/MessageMapper.java
+++ sbb-mybatis/src/main/java/com/mysite/sbbmybatis/MessageMapper.java
@@ -4,7 +4,7 @@ import org.apache.ibatis.annotations.Mapper;
 
 // 매핑 xml에 기재 된 sql을 호출하기 위한 인터페이스
 @Mapper
-public class MessageMapper {
+public interface MessageMapper {
 	Message getMessageById(Integer id);
 	void insertMessage(Message message);
 }
